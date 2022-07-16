98a67ab72a5bd3be587c9b0ae089942ffb8ab2dd
Kairo Ribeiro <kairo10@gmail.com>
2022-07-16 13:47:50 -0400

    sidebar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,20 +3,62 @@ import { NavLink } from 'react-router-dom'
 const NavBar = ({ user, handleLogout }) => {
   return (
     <>
+
       {user ?
-        <nav>
-          <h1>Foodie</h1>
-          <NavLink to="/">Posts</NavLink>
-          <NavLink to="/add">Add Post</NavLink>
-          <NavLink to="/restaurants/new">Add Restaurant +</NavLink>
-          <NavLink to="" onClick={handleLogout}>LOG OUT</NavLink>
-          <NavLink to="/changePassword">Change Password</NavLink>
+        <nav className='sidebar'>
+
+          <h1 className='logo'>Foodie</h1>
+
+          <div className='sidebarOption'>
+          <h2><span class="material-symbols-outlined">home</span>Home</h2>
+          </div>
+
+          <div className='sidebarOption'>
+          <NavLink to="/">
+          <h2><span class="material-symbols-outlined">list</span>Posts</h2>
+          </NavLink>
+          </div>
+
+          <div className='sidebarOption'>
+          <NavLink to="/add">
+          <h2><span class="material-symbols-outlined">add</span>Add Post</h2>
+          </NavLink>
+          </div>
+
+          <div className='sidebarOption'>
+          <NavLink to="/restaurants/new">
+          <h2><span class="material-symbols-outlined">restaurant</span>Add Restaurant +</h2>
+          </NavLink>
+          </div>
+
+          <div className='sidebarOption'>
+          <NavLink to="/changePassword">
+          <h2><span class="material-symbols-outlined">settings</span>Change Password</h2>
+          </NavLink>
+          </div>
+
+          <div className='sidebarOption'>
+          <NavLink to="" onClick={handleLogout}>
+          <h2><span class="material-symbols-outlined">logout</span>Logout</h2>
+          </NavLink>
+          </div>
+
         </nav>
       :
         <nav>
-          <h1>Foodie</h1>
-          <NavLink to="/login">Log In</NavLink>
-          <NavLink to="/signup">Sign Up</NavLink>
+          <h1 className='logo'>Foodie</h1>
+          <div className='sidebarOption'>
+          <NavLink to="/login">
+          <h2>Log In</h2>
+          </NavLink>
+          </div>
+
+          <div className='sidebarOption'>
+          <NavLink to="/signup">
+          <h2>Sign Up</h2>
+          </NavLink>
+          </div>
+
         </nav>
       }
     </>
